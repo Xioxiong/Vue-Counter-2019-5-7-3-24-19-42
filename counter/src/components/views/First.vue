a8ea9dcb5524312d97bf9eedf6e19c980be8ba8f
xiongwei <2894001817@qq.com>
2019-09-01 15:28:10 +0800

    dfasf

---
+++ counter/src/components/views/First.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="first-app">
+    {{msg}}
+    <confirm text="注册" @message="getMessage"></confirm>
+  </div>
+</template>
+<script>
+  import Confirm from '../sub/Confirm'
+  export default {
+    name:"First",
+    component:{
+      Confirm
+    },
+    data(){
+      return{
+        msg:"这是一个傻逼的软件"
+      };
+    },
+    methods:{
+      getMessage(val){
+        alert(val);
+      }
+    }
+  }
+</script>
